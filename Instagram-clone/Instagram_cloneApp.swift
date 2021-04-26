40a384b159a3237fef2b81e6c157535dc8e60c58
David Heath <heathdj@gmail.com>
2021-04-25 17:39:33 -0700

    Initial Commit

---
+++ Instagram-clone/Instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_cloneApp.swift
+//  Instagram-clone
+//
+//  Created by David Heath on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
